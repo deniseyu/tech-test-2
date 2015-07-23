b947206033faacb5006e16a15c29f650704116ac
Denise <yu.denise.d@gmail.com>
2015-07-23 12:58:05 +0100

    updated specs

--- spec/repartition_spec.rb
+++ spec/repartition_spec.rb
@@ -1,8 +1,8 @@
 require 'repartition'
 
 describe "Array#partition" do
-  it "returns two arrays when no block given" do
-    expect([].repartition).to eq [[], []]
+  it "returns two arrays when empty array given" do
+    expect([].repartition { |n| n == 1 }).to eq [[], []]
   end
 
   context "returns in the left array values that the block evaluates to true" do
@@ -21,11 +21,15 @@ describe "Array#partition" do
     end
 
     xit 'evaluates simple math operations' do
-      expect(ary.repartition {|i| i % 2 == 0 }).to eq [[0, 2, 4], [1, 3, 5]] 
+      expect(ary.repartition {|i| i % 2 == 0 }).to eq [[0, 2, 4], [1, 3, 5]]
     end
 
-    xit 'responds to a method passed a symbol' do
-      expect(ary.repartition(:zero?)).to eq [[0], [1, 2, 3, 4, 5]]
+    xit 'responds to a method passed in ampersand notation' do
+      expect(ary.repartition(&:zero?)).to eq [[0], [1, 2, 3, 4, 5]]
+    end
+
+    xit 'returns an enumerable when no block is given' do
+      expect(ary.repartition.class).to be Enumerator
     end
   end
 end
